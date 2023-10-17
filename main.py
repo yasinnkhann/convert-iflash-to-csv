89d7fe04b37b3452dcd6f7fe5a0cc6def2b5cd3d
Yasin Khan <yasinkhan500@gmail.com>
2023-10-16 23:12:12 -0700

    stashe

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ def get_anki_deck_data(file_path: str):
     with open(file_path, "r", encoding="utf-8") as input_file:
         lines = input_file.readlines()
         for line in lines:
-            word1, word2 = line.strip().split("\t")
+            card_set = line.strip().split("\t")
+            if len(card_set) == 2:
+                word1, word2 = card_set
+                if "&#x27;" in word1:
+                    word1 = word1.replace("&#x27;", "'")
+                if "&#x27;" in word2:
+                    word2 = word2.replace("&#x27;", "'")
 
-            if "&#x27;" in word1:
-                word1 = word1.replace("&#x27;", "'")
-            if "&#x27;" in word2:
-                word2 = word2.replace("&#x27;", "'")
+                anki_data.append((word1, word2))
 
-            anki_data.append((word1, word2))
-
-    return anki_data
+        return anki_data
 
 
 def get_google_translate_csv_or_excel_data(file_path: str):
@@ -55,8 +56,8 @@ def add_new_cards(anki_deck_file_path, google_translate_csv_or_excel_file_path):
         if anki_csv_or_excel_words not in anki_deck_data:
             print(anki_csv_or_excel_words)
             cards_to_write.append(anki_csv_or_excel_words)
-
-    save_to_csv(cards_to_write, "newly-added-portuguese-cards.csv")
+    print(len(cards_to_write))
+    # save_to_csv(cards_to_write, "newly-added-french-cards.csv")
 
 
 def convert_iflash_txt_to_csv(input_file_path: str, output_file_path: str):
@@ -94,8 +95,6 @@ def convert_iflash_docx_to_csv(input_file_path: str, output_file_path: str):
 
 
 if __name__ == "__main__":
-    add_new_cards(
-        "anki-portuguese-deck.txt", "Saved translations - Portuguese - English.csv"
-    )
+    add_new_cards("anki-french-deck.txt", "Saved translations - French - English.csv")
     # convert_iflash_docx_to_csv("iflash-input.docx", "iflash-output.csv")
     # convert_iflash_txt_to_csv("iflash-input.txt", "iflash-output.csv")
